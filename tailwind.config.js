68c4eae9a79f7bd0a1bb01ff27f46ab5ee4f65f0
davidsekielyk <david.sekielyk@gmail.com>
2021-04-07 23:47:52 -0300

    second test add tailwindcss with a plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,28 @@
 module.exports = {
-  purge: false,
+  purge: {
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.html',
+      './src/**/*.pug',
+      './src/**/*.md',
+      './docs/**/*.md',
+      './blog/**/*.md',
+    ],
+    options: {
+      whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+        'active',
+      ],
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
